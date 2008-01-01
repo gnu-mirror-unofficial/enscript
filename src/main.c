90aff5f8d6c1d6aa69be62d9ae8a12c22ccd3db7
Tim Retout <diocles@gnu.org>
2008-01-01 23:13:30 +0000

    Update bug report address, and don't hardcode it in usage().

--- src/main.c
+++ src/main.c
@@ -2625,7 +2625,7 @@ Mandatory arguments to long options are mandatory for short options too.\n\
   --word-wrap                wrap long lines from word boundaries\n\
 "));
 
-  printf (_("\nReport bugs to mtr@iki.fi.\n"));
+  printf (_("\nReport bugs to %s.\n"), PACKAGE_BUGREPORT);
 }
 
 
